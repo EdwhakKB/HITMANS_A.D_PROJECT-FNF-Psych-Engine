bd071c7e1e79120549e961624c68faee8e20c8c6
Edwhak_KB <128205303+EdwhakKB@users.noreply.github.com>
2024-05-26 00:25:53 -0500

    a
    a

--- source/Shaders.hx
+++ source/Shaders.hx
@@ -6900,3 +6900,113 @@ class SlashShaderNew extends FlxShader
         super();
     }
 }
+
+class CustomBlueShader // from yoshi engine termination port (by yoshi crafter)
+{
+	public var shader(default, null):BlueShader = new BlueShader();
+
+	public function new()
+	{
+		shader.enabled.value = [false];
+		shader.notuseX2.value = [false];
+		shader.diffX.value = [0];
+		shader.diffX2.value = [0];
+		shader.diffY.value = [-0.01];
+		shader.r.value = [255];
+		shader.g.value = [116];
+		shader.b.value = [220];
+		shader.a.value = [0.70];
+		shader.passes.value = [80];
+		shader.clipRect.value = [0, 0, 1, 1];
+	}
+}
+
+class BlueShader extends FlxFixedShader
+{
+	@:glFragmentSource('#pragma header
+
+	uniform bool enabled = false;
+	uniform bool notuseX2 = false;
+	
+	uniform float diffX = 0;
+	uniform float diffY = 0;
+	uniform float diffX2 = 0;
+	uniform float diffY2 = 0;
+	
+	uniform float r = 0;
+	uniform float g = 0;
+	uniform float b = 0;
+	uniform float a = 0;
+	
+	uniform int passes = 10;
+	
+	uniform vec4 clipRect = vec4(0, 0, 1, 1);
+	
+	// uniform float alphaReturn = 0;
+	
+	void main(){
+		vec4 color=flixel_texture2D(bitmap,openfl_TextureCoordv);
+		if(!enabled){
+			gl_FragColor=color;
+			return;
+		}
+		if(color.a<.1){
+			gl_FragColor=vec4(0.,0.,0.,0.);
+		}else{
+			/*
+			vec2 diff = vec2(diffX, diffY);
+			vec4 diffColor = flixel_texture2D(bitmap, openfl_TextureCoordv + diff);
+			*/
+			
+			// shadow alpha
+			float alpha = 0;
+			for(int i = 1; i < passes; ++i) {
+				float fPasses = passes;
+				float distX = 0;
+				if (notuseX2)
+					distX = diffX * (i / fPasses) + diffX;
+				else
+					distX = (diffX2 - diffX) * (i / fPasses) + diffX;
+				float distY = (diffY2 - diffY) * (i / fPasses) + diffY;
+				float pixelX = openfl_TextureCoordv.x + (distX * (i / fPasses));
+				float pixelY = openfl_TextureCoordv.y + (distY * (i / fPasses));
+				float a = 1.0 * ((fPasses - i) / fPasses);
+				if (pixelX > clipRect.r && pixelX < clipRect.r + clipRect.b && pixelY > clipRect.g && pixelY < clipRect.g + clipRect.a) {
+					float al = flixel_texture2D(bitmap, vec2(pixelX, pixelY)).a;
+					if (notuseX2)
+						a = (1.0 - (al / color.a)) * abs((fPasses - i - (diffX / fPasses)) / (fPasses * 2));
+					else
+						a = (1.0 - (al / color.a)) * abs((fPasses - i - (abs(diffX2 - diffX) / fPasses)) / (fPasses * 2));
+				}
+				if (alpha < a) alpha = a;
+			}
+			/*
+			for(int i=1;i<10;++i) {
+				float pixelX = openfl_TextureCoordv.x + (diffX * (i / 5));
+				float pixelY = openfl_TextureCoordv.y + (diffY * (i / 5));
+				float a = 0;
+				if (pixelX > clipRect.r && pixelX < clipRect.r + clipRect.b && pixelY > clipRect.g && pixelY < clipRect.g + clipRect.a) {
+					float alpha = flixel_texture2D(bitmap, vec2(pixelX, pixelY)).a;
+					
+					a = alpha;
+				}
+				if (a < alpha) alpha = a;
+			}
+			*/
+			
+			float shadowAlpha=(alpha)*a;
+			// float shadowAlpha = (1 - (diffColor.a)) * a;
+			
+			float nr=(color.r*(1-shadowAlpha))+(r*shadowAlpha);
+			float ng=(color.g*(1-shadowAlpha))+(g*shadowAlpha);
+			float nb=(color.b*(1-shadowAlpha))+(b*shadowAlpha);
+			float na=color.a;
+			// alphaReturn = shadowAlpha;
+			gl_FragColor=vec4(nr,ng,nb,na);
+		}
+	}')
+	public function new()
+	{
+		super();
+	}
+}
